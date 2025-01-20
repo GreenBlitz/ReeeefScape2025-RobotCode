9db2c6eb6fdf8e7f1387ca28f14fbf58e19b2fab
greenblitz <greenblitz@gmail.com>
2025-01-20 16:45:29 +0200

    danna - commiting for someone

--- src/main/java/frc/JoysticksBindings.java
+++ src/main/java/frc/JoysticksBindings.java
@@ -1,8 +1,11 @@
 package frc;
 
+import frc.joysticks.Axis;
 import frc.joysticks.JoystickPorts;
 import frc.joysticks.SmartJoystick;
 import frc.robot.Robot;
+import frc.robot.subsystems.swerve.states.SwerveState;
+import frc.robot.subsystems.swerve.states.aimassist.AimAssist;
 
 public class JoysticksBindings {
 
@@ -24,6 +27,39 @@ public class JoysticksBindings {
 
 	private static void mainJoystickButtons(Robot robot) {
 		SmartJoystick usedJoystick = MAIN_JOYSTICK;
+		SmartJoystick usedJoystick = THIRD_JOYSTICK;
+		robot.getSwerve()
+			.setDefaultCommand(
+				robot.getSwerve()
+					.getCommandsBuilder()
+					.drive(
+						() -> usedJoystick.getAxisValue(Axis.LEFT_Y),
+						() -> usedJoystick.getAxisValue(Axis.LEFT_X),
+						() -> usedJoystick.getSensitiveAxisValue(Axis.RIGHT_X)
+					)
+			);
+
+		usedJoystick.R1.whileTrue(
+			robot.getSwerve()
+				.getCommandsBuilder()
+				.driveByState(
+					() -> usedJoystick.getAxisValue(Axis.LEFT_Y),
+					() -> usedJoystick.getAxisValue(Axis.LEFT_X),
+					() -> usedJoystick.getSensitiveAxisValue(Axis.RIGHT_X),
+					SwerveState.DEFAULT_DRIVE.withAimAssist(AimAssist.SPEAKER)
+				)
+		);
+
+		usedJoystick.L1.whileTrue(
+			robot.getSwerve()
+				.getCommandsBuilder()
+				.driveByState(
+					() -> usedJoystick.getAxisValue(Axis.LEFT_Y),
+					() -> usedJoystick.getAxisValue(Axis.LEFT_X),
+					() -> usedJoystick.getSensitiveAxisValue(Axis.RIGHT_X),
+					SwerveState.DEFAULT_DRIVE.withAimAssist(AimAssist.AMP)
+				)
+		);
 		// bindings...
 	}
 
