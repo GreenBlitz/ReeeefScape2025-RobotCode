7e0fb0788e40530cda5c0ad4ab554a1ef0850e68
hillelv <hillel.vider@gmail.com>
2025-01-28 20:30:13 +0200

    nitay - cr

--- src/main/java/frc/robot/superstructure/Tolerances.java
+++ src/main/java/frc/robot/superstructure/Tolerances.java
@@ -4,8 +4,8 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Tolerances {
 
-	public static final double ELEVATOR_HEIGHT_METERS = 0.02;
+	public static final double ELEVATOR_HEIGHT_METERS = 0.05;
 
-	public static final Rotation2d ARM_POSITION = Rotation2d.fromDegrees(0.02);
+	public static final Rotation2d ARM_POSITION = Rotation2d.fromDegrees(3);
 
 }
