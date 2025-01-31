247108186ef7d3e9ff8ae36180ad158cc08e0b0a
drorta <125767200+drorta@users.noreply.github.com>
2025-01-31 23:46:47 +0200

    aa

--- src/main/java/frc/robot/superstructure/Tolerances.java
+++ src/main/java/frc/robot/superstructure/Tolerances.java
@@ -7,6 +7,7 @@ import frc.constants.field.enums.ReefSide;
 
 public class Tolerances {
 
+
 	public static final double ELEVATOR_HEIGHT_METERS = 0.01;
 
 	public static final Rotation2d ARM_POSITION = Rotation2d.fromDegrees(1.5);
