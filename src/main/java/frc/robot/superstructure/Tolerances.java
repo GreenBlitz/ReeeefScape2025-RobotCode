e614db801510fc4e23a4ded19698577baa401d51
drorta <125767200+drorta@users.noreply.github.com>
2025-01-31 23:49:28 +0200

    maybe

--- src/main/java/frc/robot/superstructure/Tolerances.java
+++ src/main/java/frc/robot/superstructure/Tolerances.java
@@ -3,6 +3,7 @@ package frc.robot.superstructure;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.constants.field.Field;
+import frc.constants.field.enums.ReefSide;
 
 public class Tolerances {
 
@@ -11,12 +12,26 @@ public class Tolerances {
 
 	public static final Rotation2d ARM_POSITION = Rotation2d.fromDegrees(1.5);
 
-	// todo - should be used relative to reef side angle (tolerances, deadbands)
-	public static final Pose2d BRANCH_SCORE_POSE = new Pose2d(0.05, 0.05, Rotation2d.fromDegrees(3));
-	public static final Pose2d BRANCH_SCORE_VELOCITY_DEADBANDS = new Pose2d(0.1, 0.1, Rotation2d.fromRadians(0.1));
+	private static final Pose2d BRANCH_SCORE_POSE = new Pose2d(0.05, 0.05, Rotation2d.fromDegrees(3));
+	private static final Pose2d BRANCH_SCORE_VELOCITY_DEADBANDS = new Pose2d(0.1, 0.1, Rotation2d.fromRadians(0.1));
 
-	// todo - should be used relative to reef side angle (tolerances, deadbands)
-	public static final Pose2d L1_SCORE_POSE = new Pose2d(Field.REEF_SIDE_LENGTH_METERS / 2.0, 0.05, Rotation2d.fromDegrees(3));
-	public static final Pose2d L1_SCORE_VELOCITY_DEADBANDS = new Pose2d(1, 0.1, Rotation2d.fromRadians(0.1));
+	public static Pose2d branchScorePose(ReefSide reefSide) {
+		return BRANCH_SCORE_POSE.rotateBy(Field.getReefSideMiddle(reefSide).getRotation());
+	}
+
+	public static Pose2d branchScoreVelocityDeadbands(ReefSide reefSide) {
+		return BRANCH_SCORE_VELOCITY_DEADBANDS.rotateBy(Field.getReefSideMiddle(reefSide).getRotation());
+	}
+
+	private static final Pose2d L1_SCORE_POSE = new Pose2d(Field.REEF_SIDE_LENGTH_METERS / 2.0, 0.05, Rotation2d.fromDegrees(3));
+	private static final Pose2d L1_SCORE_VELOCITY_DEADBANDS = new Pose2d(1, 0.1, Rotation2d.fromRadians(0.1));
+
+	public static Pose2d l1ScorePose(ReefSide reefSide) {
+		return L1_SCORE_POSE.rotateBy(Field.getReefSideMiddle(reefSide).getRotation());
+	}
+
+	public static Pose2d l1ScoreVelocityDeadbands(ReefSide reefSide) {
+		return L1_SCORE_VELOCITY_DEADBANDS.rotateBy(Field.getReefSideMiddle(reefSide).getRotation());
+	}
 
 }
