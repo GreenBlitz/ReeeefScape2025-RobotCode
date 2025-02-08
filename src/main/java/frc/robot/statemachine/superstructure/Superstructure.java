1dbae82b0618eaeb1dbbb9c507455a80e95a1286
Nitay4 <nitay@rotbart.co.il>
2025-02-08 12:57:32 +0200

    nitay - cr

--- src/main/java/frc/robot/statemachine/superstructure/Superstructure.java
+++ src/main/java/frc/robot/statemachine/superstructure/Superstructure.java
@@ -1,6 +1,5 @@
 package frc.robot.statemachine.superstructure;
 
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.DeferredCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
