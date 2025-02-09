882e84f4dcac1178eabeaaefbec6392e8767a232
greenblitz <greenblitz@gmail.com>
2025-02-09 17:39:30 +0200

    Romy - fixed the states

--- src/main/java/frc/robot/scoringhelpers/ScoringHelpers.java
+++ src/main/java/frc/robot/scoringhelpers/ScoringHelpers.java
@@ -5,10 +5,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.constants.field.Field;
 import frc.constants.field.enums.Branch;
+import frc.constants.field.enums.CoralStation;
+import frc.constants.field.enums.ReefSide;
 
 public class ScoringHelpers {
 
 	public static Branch targetBranch = Branch.C;
+
+	public static ReefSide targetReefSide = ReefSide.B;
+
+	public static CoralStation targetCoralStation = CoralStation.LEFT;
 
 	public static Pose2d getRobotScoringPose(Branch branch, double distanceFromBranchMeters) {
 		Translation2d branchTranslation = Field.getCoralPlacement(branch);
