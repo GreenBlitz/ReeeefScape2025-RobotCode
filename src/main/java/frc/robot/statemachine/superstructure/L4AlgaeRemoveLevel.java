3464bd91b363c18dbfcd457a0f13cd73f8475c06
YHerm <0549405422.yh@gmail.com>
2025-02-21 22:31:26 +0200

    merged

--- src/main/java/frc/robot/statemachine/superstructure/L4AlgaeRemoveLevel.java
+++ src/main/java/frc/robot/statemachine/superstructure/L4AlgaeRemoveLevel.java
@@ -6,77 +6,77 @@ import frc.robot.subsystems.endeffector.EndEffectorState;
 
 public enum L4AlgaeRemoveLevel {
 
-    LOW(
-        ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
-        ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
-        ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        EndEffectorState.ALGAE_INTAKE
-    ),
-    HIGH(
-        ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
-        ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
-        ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        ArmState.ALGAE_REMOVE_FROM_L4,
-        EndEffectorState.ALGAE_INTAKE
-    );
+	LOW(
+		ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
+		ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
+		ElevatorState.LOW_ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		EndEffectorState.ALGAE_INTAKE
+	),
+	HIGH(
+		ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
+		ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
+		ElevatorState.HIGH_ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		ArmState.ALGAE_REMOVE_FROM_L4,
+		EndEffectorState.ALGAE_INTAKE
+	);
 
-    private final ElevatorState elevatorState;
-    private final ElevatorState preElevatorState;
-    private final ElevatorState postElevatorState;
-    private final ArmState armState;
-    private final ArmState preArmState;
-    private final ArmState postArmState;
-    private final EndEffectorState endEffectorState;
+	private final ElevatorState elevatorState;
+	private final ElevatorState preElevatorState;
+	private final ElevatorState postElevatorState;
+	private final ArmState armState;
+	private final ArmState preArmState;
+	private final ArmState postArmState;
+	private final EndEffectorState endEffectorState;
 
-    L4AlgaeRemoveLevel(
-        ElevatorState elevatorState,
-        ElevatorState preElevatorState,
-        ElevatorState postElevatorState,
-        ArmState armState,
-        ArmState preArmState,
-        ArmState postArmState,
-        EndEffectorState endEffectorState
-    ) {
-        this.elevatorState = elevatorState;
-        this.preElevatorState = preElevatorState;
-        this.postElevatorState = postElevatorState;
-        this.armState = armState;
-        this.preArmState = preArmState;
-        this.postArmState = postArmState;
-        this.endEffectorState = endEffectorState;
-    }
+	L4AlgaeRemoveLevel(
+		ElevatorState elevatorState,
+		ElevatorState preElevatorState,
+		ElevatorState postElevatorState,
+		ArmState armState,
+		ArmState preArmState,
+		ArmState postArmState,
+		EndEffectorState endEffectorState
+	) {
+		this.elevatorState = elevatorState;
+		this.preElevatorState = preElevatorState;
+		this.postElevatorState = postElevatorState;
+		this.armState = armState;
+		this.preArmState = preArmState;
+		this.postArmState = postArmState;
+		this.endEffectorState = endEffectorState;
+	}
 
-    public ElevatorState getElevatorState() {
-        return elevatorState;
-    }
+	public ElevatorState getElevatorState() {
+		return elevatorState;
+	}
 
-    public ElevatorState getPreElevatorState() {
-        return preElevatorState;
-    }
+	public ElevatorState getPreElevatorState() {
+		return preElevatorState;
+	}
 
-    public ElevatorState getPostElevatorState() {
-        return postElevatorState;
-    }
+	public ElevatorState getPostElevatorState() {
+		return postElevatorState;
+	}
 
-    public ArmState getArmState() {
-        return armState;
-    }
+	public ArmState getArmState() {
+		return armState;
+	}
 
-    public ArmState getPreArmState() {
-        return preArmState;
-    }
+	public ArmState getPreArmState() {
+		return preArmState;
+	}
 
-    public ArmState getPostArmState() {
-        return postArmState;
-    }
+	public ArmState getPostArmState() {
+		return postArmState;
+	}
 
-    public EndEffectorState getEndEffectorState() {
-        return endEffectorState;
-    }
+	public EndEffectorState getEndEffectorState() {
+		return endEffectorState;
+	}
 
 }
