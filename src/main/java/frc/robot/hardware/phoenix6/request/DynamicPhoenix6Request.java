8cd71eb054f6d227faa5d5b454a41d2e476e6a39
RJ0907 <ranelj2008@gmail.com>
2025-02-22 23:44:32 +0200

    Ran - added crucial functions

--- src/main/java/frc/robot/hardware/phoenix6/request/DynamicPhoenix6Request.java
+++ src/main/java/frc/robot/hardware/phoenix6/request/DynamicPhoenix6Request.java
@@ -5,31 +5,37 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 import java.util.function.Consumer;
 
-public class DynamicPhoenix6Request extends Phoenix6FeedForwardRequest{
+public class DynamicPhoenix6Request extends Phoenix6FeedForwardRequest {
     private final Consumer<Double> setVelocity;
     private double velocity;
     private final Consumer<Double> setAcceleration;
     private double acceleration;
-    DynamicPhoenix6Request(
-            Rotation2d defaultSetPoint,
-            ControlRequest controlRequest,
-            Consumer<Rotation2d> setSetPoint,
-            Consumer<Double> setFeedForward,
-            double defaultArbitraryFeedForward,
-            Consumer<Double> setVelocity,
-            double velocity,
-            Consumer<Double> setAcceleration,
-            double acceleration
-    ) {
-        super(
-                defaultSetPoint,
-                controlRequest,
-                setSetPoint, setFeedForward,
-                defaultArbitraryFeedForward
-        );
+
+    DynamicPhoenix6Request(Rotation2d defaultSetPoint, ControlRequest controlRequest, Consumer<Rotation2d> setSetPoint, Consumer<Double> setFeedForward, double defaultArbitraryFeedForward, Consumer<Double> setVelocity, double velocity, Consumer<Double> setAcceleration, double acceleration) {
+        super(defaultSetPoint, controlRequest, setSetPoint, setFeedForward, defaultArbitraryFeedForward);
         this.setVelocity = setVelocity;
         this.velocity = velocity;
         this.setAcceleration = setAcceleration;
         this.acceleration = acceleration;
+    }
+
+    public DynamicPhoenix6Request withVelocity(double newVelocity) {
+        setVelocity.accept(newVelocity);
+        this.velocity = newVelocity;
+        return this;
+    }
+
+    public double getVelocity() {
+        return this.velocity;
+    }
+
+    public DynamicPhoenix6Request withAcceleration(double newAcceleration) {
+        setAcceleration.accept(newAcceleration);
+        this.acceleration = newAcceleration;
+        return this;
+    }
+
+    public double getAcceleration() {
+        return this.acceleration;
     }
 }
