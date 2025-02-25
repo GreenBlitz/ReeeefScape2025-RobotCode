bea25c788246c57384632bcebb57e455cb5ae14c
hillelv <hillel.vider@gmail.com>
2025-02-25 12:15:45 +0200

    hillel - choose

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.RobotManager;
 import frc.robot.autonomous.AutonomousConstants;
@@ -257,6 +258,9 @@ public class Robot {
 	}
 
 	public PathPlannerAutoWrapper getAuto() {
+		if (!preBuiltAutosChooser.getChosenValue().getName().equals("None")) {
+			return preBuiltAutosChooser.getChosenValue();
+		}
 		return PathPlannerAutoWrapper.chainAutos(
 			startingPointAndWhereToScoreFirstObjectChooser.getChosenValue(),
 			whereToIntakeSecondObjectChooser.getChosenValue(),
