11ac839592c2bc501a7bab1eacecbda7b3df13c9
Nitay4 <nitay@rotbart.co.il>
2025-09-25 13:06:53 +0300

    Nitay - added a* finder

---
+++ src/main/java/frc/robot/statemachine/AStarFinder/AStarFinder.java
@@ -0,0 +1,81 @@
+package frc.robot.statemachine.AStarFinder;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+import frc.robot.statemachine.RobotState;
+
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+public class AStarFinder {
+
+	private static BiFunction<RobotState, RobotState, Double> hurristicSupplier = (currentState, targetState) -> 1.0;
+
+	public static Command findSequence(Pair<RobotState, RobotState> states, Robot robot) {
+		StateNode startingState = new StateNode(states.getFirst(), 0, hurristicSupplier.apply(states.getFirst(), states.getSecond()), null);
+		RobotState targetState = states.getSecond();
+
+		StateNode currentState = startingState;
+
+		LinkedList<StateNode> checkedStates = new LinkedList<>();
+		checkedStates.add(startingState);
+
+		while (!currentState.getState().equals(targetState)) {
+			StateNode temp = checkedStates.get(0);
+
+			for (int i = 1; i < checkedStates.size(); i++) {
+				if (temp.getFCost() > checkedStates.get(i).getFCost()) {
+					temp = checkedStates.get(i);
+				}
+			}
+
+			currentState = temp;
+
+			RobotState[] neighbors = currentState.getState().getNeighbors();
+			for (int i = 0; i < neighbors.length; i++) {
+				if (neighbors[i].equals(targetState)) {
+					currentState = new StateNode(targetState, Integer.MAX_VALUE, 0, currentState);
+					break;
+				}
+
+				StateNode currentNeighbor;
+				if (checkForState(checkedStates, neighbors[i]).get() != null) {
+					currentNeighbor = checkForState(checkedStates, neighbors[i]).get();
+					if (currentNeighbor.getGCost() > currentState.getGCost() + 1) {
+						currentNeighbor.setParent(currentState);
+					}
+				} else {
+					currentNeighbor = new StateNode(
+						neighbors[i],
+						currentState.getGCost() + 1,
+						hurristicSupplier.apply(neighbors[i], targetState),
+						currentState
+					);
+                    checkedStates.add(currentNeighbor);
+				}
+			}
+		}
+
+        Command path = robot.getRobotCommander().setState(targetState);
+
+        while (!currentState.getParent().equals(currentState)){
+            currentState = currentState.getParent();
+            path = path.beforeStarting(robot.getRobotCommander().setState(currentState.getState()));
+        }
+
+        return path;
+
+	}
+
+	private static Optional<StateNode> checkForState(LinkedList<StateNode> checkedStates, RobotState current) {
+		for (int i = 0; i < checkedStates.size(); i++) {
+			if (checkedStates.get(i).getState().equals(current)) {
+				return Optional.of(checkedStates.get(i));
+			}
+		}
+		return Optional.empty();
+	}
+
+}
--- src/main/java/frc/robot/statemachine/RobotState.java
+++ src/main/java/frc/robot/statemachine/RobotState.java
@@ -2,33 +2,43 @@ package frc.robot.statemachine;
 
 public enum RobotState {
 
-	STAY_IN_PLACE,
+	STAY_IN_PLACE(new RobotState[] {}),
 	DRIVE,
-	INTAKE_WITH_AIM_ASSIST,
-	INTAKE_WITHOUT_AIM_ASSIST,
-	CORAL_OUTTAKE,
-	ALIGN_REEF,
-	ARM_PRE_SCORE,
-	PRE_SCORE,
-	SCORE_WITHOUT_RELEASE,
-	SCORE,
-	ALGAE_REMOVE,
-	ALGAE_OUTTAKE_FROM_END_EFFECTOR,
-	ALGAE_OUTTAKE_FROM_INTAKE,
+	INTAKE_WITH_AIM_ASSIST(new RobotState[] {}),
+	INTAKE_WITHOUT_AIM_ASSIST(new RobotState[] {}),
+	CORAL_OUTTAKE(new RobotState[] {}),
+	ALIGN_REEF(new RobotState[] {}),
+	ARM_PRE_SCORE(new RobotState[] {}),
+	PRE_SCORE(new RobotState[] {}),
+	SCORE_WITHOUT_RELEASE(new RobotState[] {}),
+	SCORE(new RobotState[] {}),
+	ALGAE_REMOVE(new RobotState[] {}),
+	ALGAE_OUTTAKE_FROM_END_EFFECTOR(new RobotState[] {}),
+	ALGAE_OUTTAKE_FROM_INTAKE(new RobotState[] {}),
 	ALGAE_INTAKE,
 	TRANSFER_ALGAE_TO_END_EFFECTOR,
 	HOLD_ALGAE,
-	AUTO_PRE_NET,
+	AUTO_PRE_NET(new RobotState[] {}),
 	PRE_NET,
 	NET,
-	PROCESSOR_SCORE,
-	PRE_CLIMB_WITH_AIM_ASSIST,
-	PRE_CLIMB_WITHOUT_AIM_ASSIST,
-	CLIMB_WITHOUT_LIMIT_SWITCH,
-	CLIMB_WITH_LIMIT_SWITCH,
-	MANUAL_CLIMB,
-	EXIT_CLIMB,
-	STOP_CLIMB,
-	CLOSE_CLIMB;
+	PROCESSOR_SCORE(new RobotState[] {}),
+	PRE_CLIMB_WITH_AIM_ASSIST(new RobotState[] {}),
+	PRE_CLIMB_WITHOUT_AIM_ASSIST(new RobotState[] {}),
+	CLIMB_WITHOUT_LIMIT_SWITCH(new RobotState[] {}),
+	CLIMB_WITH_LIMIT_SWITCH(new RobotState[] {}),
+	MANUAL_CLIMB(new RobotState[] {}),
+	EXIT_CLIMB(new RobotState[] {}),
+	STOP_CLIMB(new RobotState[] {}),
+	CLOSE_CLIMB(new RobotState[] {});
+
+	private RobotState[] neighbors;
+
+	RobotState(RobotState[] neighbors) {
+		this.neighbors = neighbors;
+	}
+
+	public RobotState[] getNeighbors() {
+		return neighbors;
+	}
 
 }
